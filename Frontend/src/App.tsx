d26ba1c78fc805a15f919097001c3f1e445c1759
Nicklas Hansen <nhy@capasystems.com>
2025-09-19 10:51:48 +0200

    Make so if you press on the Title name then it navigates back to the list

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,41 +1,48 @@
 import "./App.css";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import DestinationDetail from "./pages/DestinationsDetails";
 
 const App = () => {
   return (
-    <div className="container mx-auto px-4 pt-8">
-      <Header />
-      <Content />
-    </div>
+    <BrowserRouter>
+      <div className="container mx-auto px-4 pt-8">
+        <Header />
+        <Content />
+      </div>
+    </BrowserRouter>
   );
 };
 
 const Content = () => {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/destinations/:destinationId"
-              element={<DestinationDetail />}
-            />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/destinations/:destinationId"
+            element={<DestinationDetail />}
+          />
+        </Routes>
+      </main>
+    </div>
   );
 };
 export default App;
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="">
-      <h1 className="text-3xl font-bold text-gray-800 mb-6">CapaBNBs</h1>
+      <h1
+        onClick={() => navigate("/")}
+        className="text-3xl font-bold text-gray-800 mb-6 cursor-pointer hover:text-gray-600 transition-colors"
+      >
+        CapaBNBs
+      </h1>
     </div>
   );
 };
