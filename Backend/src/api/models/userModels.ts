d4352b08ba55f5c874b1b4369280e7cee65d9841
Nicklas Hansen <nhy@capasystems.com>
2025-09-25 09:47:07 +0200

    Revert "Backend for login v1"

--- Backend/src/api/models/userModels.ts
+++ Backend/src/api/models/userModels.ts
@@ -1,44 +1,19 @@
-import mongoose, { Schema, Document } from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose, { Schema } from "mongoose";
 
-interface User extends Document {
+interface User {
   username: string;
   email: string;
   password: string;
   phone: number;
   avatar?: string;
-  comparePassword(candidatePassword: string): Promise<boolean>;
 }
 
-const userSchema: Schema = new Schema(
-  {
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    phone: { type: Number, required: true },
-    avatar: { type: String, required: false },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-userSchema.pre<User>("save", async function (next) {
-  if (!this.isModified("password")) return next();
-
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error as Error);
-  }
+const userSchema: Schema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: Number, required: true },
+  avatar: { type: String, required: false },
 });
-
-userSchema.methods.comparePassword = async function (
-  candidatePassword: string
-): Promise<boolean> {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 export const UserModel = mongoose.model<User>("User", userSchema);
