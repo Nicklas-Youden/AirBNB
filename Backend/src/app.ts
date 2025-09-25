cf58a9e32aedf2555a2de84cf7c886c8855ffb7c
Nicklas Hansen <nhy@capasystems.com>
2025-09-25 09:50:13 +0200

    Revert "Revert "Backend for login v1""

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import airBnbDestinationsRoutes from "./api/routes/airBnbDestinationsRoutes";
+import userRoutes from "./api/routes/userRoutes";
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,7 @@ app.use(express.json());
 // Serve static files from public directory
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.use("/api", airBnbDestinationsRoutes);
+app.use("/api/destinations", airBnbDestinationsRoutes);
+app.use("/api/users", userRoutes);
 
 export default app;
